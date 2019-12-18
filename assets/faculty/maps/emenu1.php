53843e3ed27a513f3ea87afc3f2c58c18706d802
Sanath L S <sanathlslokanathapura@gmail.com>
2019-12-18 14:09:24 +0530

    pbsa Save Completed

---
+++ assets/faculty/maps/emenu1.php
@@ -0,0 +1,288 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>skills</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="skills" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/amcharts.js"></script>	
+<script src="js/serial.js"></script>	
+<script src="js/light.js"></script>	
+<script src="js/radar.js"></script>	
+<link href="css/barChart.css" rel='stylesheet' type='text/css' />
+<link href="css/fabochart.css" rel='stylesheet' type='text/css' />
+<!--clock init-->
+<script src="js/css3clock.js"></script>
+<!--Easy Pie Chart-->
+<!--skycons-icons-->
+<script src="js/skycons.js"></script>
+
+<script src="js/jquery.easydropdown.js"></script>
+<style>
+.tini-time-line1
+{
+margin-left:20%;
+
+}
+</style>
+<!--//skycons-icons-->
+</head> 
+<body>
+   <div class="page-container">
+   <!--/content-inner-->
+	<div class="left-content">
+	   <div class="inner-content">
+		<!-- header-starts -->
+			<div class="header-section">
+						<!--menu-right-->
+						<div class="top_menu">
+						        
+									
+													   <div id="" class="wrapper-dropdown-2" >
+																			<br>	<br>	<br>
+																		</div>
+																	
+										
+									       
+									   							   		
+							<div class="clearfix"></div>	
+								
+							
+							<div class="clearfix"></div>	
+							<!--//profile_details-->
+						</div>
+						<!--//menu-right-->
+					<div class="clearfix"></div>
+				</div>
+					<!-- //header-ends -->
+						<div class="outter-wp">
+								<!--custom-widgets-->
+											
+												
+												
+												
+												
+												
+												<!--//custom-widgets-->
+												<!--/candile-->
+													<div class="candile"> 
+															
+																	
+																
+															    <div id="center"><div id="fig">
+														
+
+														</div>
+																				
+															</div>
+															
+														</div>
+													<!--/candile-->
+													
+												<!--/charts-->
+																													<div class="charts">
+												  <div class="chrt-inner">
+												    <div class="chrt-bars">
+														
+														
+															<div id="chart2"></div>
+															
+														
+															
+																<div class="clearfix"> </div>
+															</div>										
+												<!--/float-charts-->
+												<div class="pie">
+													
+														<h3 class="sub-tittle"></h3>
+														<div id="chartdiv2"></div>	
+														
+				
+														
+														
+															<h3 class="sub-tittle"></h3>
+														<div id="chartdiv4"></div>	
+														     
+													        
+														
+																<div class="clearfix"> </div>
+														</div>
+												<div class="area">
+											
+															<h3 class="sub-tittle"></h3>
+															 <div style="area">
+																
+																</div>
+															
+															
+															
+															<h3 class="sub-tittle"></h3>
+															    <div id="chartdiv1"></div>	
+															
+																
+																<div class="clearfix"></div>
+												</div>
+													<!--//weather-charts-->
+														<div class="graph-visualization">
+															
+																<h3 class="sub-tittle"></h3>
+																	<div class="weather">
+																	<div class="weather-top">
+																		<div class="weather-top-left">
+																			
+																			
+																			<div class="clearfix"></div>
+																			</div>
+																
+																		
+																		</div>
+																		<div class="weather-top-right">
+																			
+																		</div>
+																		<div class="clearfix"> </div>
+																	</div>
+																	<div class="weather-bottom">
+															<div class="weather-bottom1">
+																<div class="weather-head">
+															
+																<div class="bottom-head">
+															
+																</div>
+															</div>
+															</div>
+															<div class="weather-bottom1 ">
+																<div class="weather-head">
+														
+															</div>
+															</div>
+															<div class="weather-bottom1">
+																<div class="weather-head">
+																
+															</div>
+															</div>
+															<div class="weather-bottom1 ">
+																<div class="weather-head">
+														
+															
+															
+																</div>
+															</div>
+															<div class="clearfix"> </div>
+														</div>
+
+															
+
+															</div>
+														
+															<div class="clearfix"> </div>
+														</div>
+
+												<!--//charts-->
+												<div class="area-charts">
+												
+														
+															<div class="clearfix"></div>		
+													
+										<!--/bottom-grids-->		 
+								
+										<div class="dev-table">    
+											
+                                        
+										<div class="clearfix"></div>		
+										
+										</div>
+									
+									<!--//bottom-grids-->
+									
+									</div>
+									<!--/charts-inner-->
+									</div>
+										<!--//outer-wp-->
+									</div>
+									
+								</div>
+							</div>
+				<!--//content-inner-->
+			<!--/sidebar-menu-->
+				<div class="sidebar-menu">
+					<header class="logo">
+					<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> <a href="index.html"> <span id="logo"> <h1>SDMIT</h1></span> 
+					<!--<img id="logo" src="" alt="Logo"/>--> 
+				  </a> 
+				</header>
+			<div style="border-top:1px solid rgba(69, 74, 84, 0.7)"></div>
+			<!--/down-->
+							<div class="down">	
+									  <a href="index.html"><img src="images/a.png"></a>
+									  <a href="index.html"><span class=" name-caret">Mr. Abhishek</span></a>
+									 <p>Employee </p>
+									<ul>
+									<li><a class="tooltips" href="index.html"><span>Profile</span><i class="lnr lnr-user"></i></a></li>
+										
+										<li><a class="tooltips" href="../start/index.html"><span>Log out</span><i class="lnr lnr-power-switch"></i></a></li>
+										</ul>
+									</div>
+							   <!--//down-->
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="emenu.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span></a></li>
+										 <li id="menu-academico" ><a href="rating.php"><i class="lnr lnr-pencil"></i> <span>Rate HODs</span> </a>
+										   
+										</li>
+										 <li id="menu-academico" ><a href="viewpro.php"><i class="fa fa-file-text-o"></i> <span>View Current Projects</span> </a>
+											</li>
+									<li><a href="viewskills.php"><i class="lnr lnr-layers"></i> <span>View Your Performance </span></a></li>
+									<li id="menu-academico" ><a href="viewemp.php"><i class="fa fa-users"></i> <span>Apply for Project
+										  
+									 </li>
+									
+								</div>
+							  </div>
+							  <div class="clearfix"></div>		
+							</div>
+							<script>
+							var toggle = true;
+										
+							$(".sidebar-icon").click(function() {                
+							  if (toggle)
+							  {
+								$(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+								$("#menu span").css({"position":"absolute"});
+							  }
+							  else
+							  {
+								$(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+								setTimeout(function() {
+								  $("#menu span").css({"position":"relative"});
+								}, 400);
+							  }
+											
+											toggle = !toggle;
+										});
+							</script>
+<!--js -->
+<link rel="stylesheet" href="css/vroom.css">
+<script type="text/javascript" src="js/vroom.js"></script>
+<script type="text/javascript" src="js/TweenLite.min.js"></script>
+<script type="text/javascript" src="js/CSSPlugin.min.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
